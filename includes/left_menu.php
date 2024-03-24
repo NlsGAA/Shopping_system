d8d5967c9f860d013858f1681ef580ee66946d97
NlsGAA <nicolasmaerico@gmail.com>
2024-03-24 11:32:59 -0300

    style: add style in delete cart itens button and modal for confirmation

--- includes/left_menu.php
+++ includes/left_menu.php
@@ -14,9 +14,13 @@ $cart_itens = $productDao->takeItensByUserId($_SESSION['logado']['id']);
         <div class="content_cart_body">
             <?php if ($cart_itens !== 0 && is_array($cart_itens)) : ?>
 
-                <form action="http://localhost/sistema_de_compra/php_action/cartDeleteAllItens.php" method="POST">
-                    <button name="deleteAllItens">Esvaziar carrinho</button>
-                </form>
+                <button name="deleteAllItens" id="deleteCart" type="button" data-bs-toggle="modal" data-bs-target="#cartDeleteItens">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                    </svg>
+                    Esvaziar carrinho
+                </button>
 
                 <?php
                 $total_price = 0;
@@ -57,6 +61,24 @@ $cart_itens = $productDao->takeItensByUserId($_SESSION['logado']['id']);
                                         <form action="php_action/cartItenDelete.php" method="POST">
                                             <input type="hidden" name="id" value="<?= $product_detail->getId() ?>">
                                             <button type="submit" name="btn_delete_item" class="btn btn-danger">Excluir</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Modal Delete Cart Itens -->
+                        <div class="modal fade" id="cartDeleteItens" tabindex="-1" aria-labelledby="itemModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="itemModalLabel">Esvaziar carrinho?</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                                        <form action="http://localhost/sistema_de_compra/php_action/cartDeleteAllItens.php" method="POST">
+                                            <button type="submit" class="btn btn-danger" name="deleteAllItens">Esvaziar</button>
                                         </form>
                                     </div>
                                 </div>
