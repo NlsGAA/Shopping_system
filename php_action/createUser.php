f235d5fd519caa09ab73aea6afc94959d3bfad82
NlsGAA <nicolasmaerico@gmail.com>
2024-05-01 20:55:37 -0300

    Refactor: modify the structure of the create user file

--- php_action/createUser.php
+++ php_action/createUser.php
@@ -3,51 +3,63 @@ session_start();
 
 include_once __DIR__ . ("/db_connect.php");
 include_once __DIR__ . ("/../dao/RegisterUserDAO.php");
-$user = new RegisterUserDAO($pdo);
 
-$type_user = filter_input(INPUT_POST, 'type');
-$cpf = filter_input(INPUT_POST, 'cpf');
-$cnpj = filter_input(INPUT_POST, 'cnpj');
-$birthdate = filter_input(INPUT_POST, 'birthdate');
-$address = filter_input(INPUT_POST, 'address');
-$companyAddress = filter_input(INPUT_POST, 'companyAddress');
-$companyName = filter_input(INPUT_POST, 'companyName');
-$fantasyName = filter_input(INPUT_POST, 'fantasyName');
-$email = filter_input(INPUT_POST, 'email');
-$password = filter_input(INPUT_POST, 'password');
+function createUser($userData, $dao) {
+    $physical = new User;
+    $physical->setTypeUser($userData['type_user']);
+    $physical->setCpf($userData['cpf']);
+    $physical->setBirthdate($userData['birthdate']);
+    $physical->setAddress($userData['address']);
+    $physical->setEmail($userData['email']);
+    $physical->setPassword($userData['password']);
+    $dao->createUser($userData['physical']);
+}
+
+function createCompany($userData, $dao) {
+    $company = new Company;
+    $company->setTypeUser($userData['type_user']);
+    $company->setCnpj($userData['cnpj']);
+    $company->setCompanyAddress($userData['companyAddress']);
+    $company->setCompanyName($userData['companyName']);
+    $company->setFantasyName($userData['fantasyName']);
+    $company->setEmail($userData['email']);
+    $company->setPassword($userData['password']);
+    $dao->createCompany($userData['company']);
+}
+
 
 if (isset($_POST['btn_register'])) {
+    $userData = [
+        'type_user' => filter_input(INPUT_POST, 'type'),
+        'cpf' => filter_input(INPUT_POST, 'cpf'),
+        'cnpj' => filter_input(INPUT_POST, 'cnpj'),
+        'birthdate' => filter_input(INPUT_POST, 'birthdate'),
+        'address' => filter_input(INPUT_POST, 'address'),
+        'companyAddress' => filter_input(INPUT_POST, 'companyAddress'),
+        'companyName' => filter_input(INPUT_POST, 'companyName'),
+        'fantasyName' => filter_input(INPUT_POST, 'fantasyName'),
+        'email' => filter_input(INPUT_POST, 'email'),
+        'password' => filter_input(INPUT_POST, 'password') 
+    ];
 
-    $erro = $user->validatePhysicalAvailability($email);
-    if (empty($erro)) {
-        $physical = new User;
-        $physical->setTypeUser($type_user);
-        $physical->setCpf($cpf);
-        $physical->setBirthdate($birthdate);
-        $physical->setAddress($address);
-        $physical->setEmail($email);
-        $physical->setPassword($password);
-        $user->createUser($physical);
+    $dao = new RegisterUserDAO($pdo);
+
+    $userError = $user->validatePhysicalAvailability($userData['email']);
+    $companyError = $user->validateCompanyAvailability($userData['cnpj']);
+
+    if(empty($userError)){
+        createUser($userData, $dao);
+    }
+
+    if(empty($companyError)){
+        createCompany($userData, $dao);
     }
 
 
-    $erro = $user->validateCompanyAvailability($cnpj);
-    if (empty($erro)) {
-        $company = new Company;
-        $company->setTypeUser($type_user);
-        $company->setCnpj($cnpj);
-        $company->setCompanyAddress($companyAddress);
-        $company->setCompanyName($companyName);
-        $company->setFantasyName($fantasyName);
-        $company->setEmail($email);
-        $company->setPassword($password);
-        $user->createCompany($company);
-    }
-
-    if (!empty($erro)) {
+    if (!empty($userError) || !empty($companyError)) {
         $_SESSION['message'] = array(
             'status' => true,
-            'message' => $erro,
+            'message' => $physicalError ?? $companyError,
             'cod' => 00004
         );
         header('Location: ../register.php');
